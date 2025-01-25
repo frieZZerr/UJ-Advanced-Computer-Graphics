frieZZerr/UJ-Advanced-Computer-Graphics

commit d90b6319ac75c7dc3deb74843f7b3e5b183e0130

Refactor objects classes
- Created classes for Grass, Target and Projectile
- Moved static radius variables to private field
- Target now spawns in a specified random position
- Created multiple target handling

- [NEED-FIX] Targets can spawn in each other

project_2/src/input/InputHandler.cpp
@@ -45,7 +45,7 @@ bool InputHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdap
             _cannon->setFireKeyHeld(false);
             double holdDuration = ea.getTime() - _cannon->getFirePressStartTime();
 
-            _cannon->fire(static_cast<float>(holdDuration), _sceneRoot, _target);
+            _cannon->fire(static_cast<float>(holdDuration), _sceneRoot, _targets);
 
             return true;
         }

project_2/src/input/InputHandler.h
@@ -2,24 +2,26 @@
 #define INPUT_HANDLER_H
 
 #include "scene/objects/Cannon.h"
+#include "scene/objects/Target.h"
 
 #include <osg/Group>
 #include <osg/PositionAttitudeTransform>
 #include <osgGA/GUIEventHandler>
+#include <vector>
 
 class InputHandler : public osgGA::GUIEventHandler {
 public:
-    InputHandler(osg::Group* sceneRoot, Cannon* cannon, osg::PositionAttitudeTransform* target)
+    InputHandler(osg::Group* sceneRoot, Cannon* cannon, std::vector<osg::ref_ptr<Target>>& targets)
         : _sceneRoot(sceneRoot),
           _cannon(cannon),
-          _target(target) {};
+          _targets(targets) {};
 
     virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa);
 
 private:
     osg::Group* _sceneRoot;
     osg::ref_ptr<Cannon> _cannon;
-    osg::ref_ptr<osg::PositionAttitudeTransform> _target;
+    std::vector<osg::ref_ptr<Target>> _targets;
 };
 
 #endif // INPUT_HANDLER_H

project_2/src/scene/Scene.cpp
@@ -1,5 +1,9 @@
 #include "scene/Scene.h"
+#include "scene/objects/Grass.h"
 #include "scene/objects/Cannon.h"
+#include "scene/objects/Target.h"
+
+#include <osg/Vec3>
 
 Scene::Scene() {
     _root = new osg::Group();
@@ -7,11 +11,16 @@ Scene::Scene() {
 }
 
 void Scene::buildScene() {
-    _root->addChild(createGrass());
+    _grass = new Grass();
+    _root->addChild(_grass->getTransform());
 
-    _cannon = new Cannon();
+    osg::Vec3 cannonPosition = osg::Vec3(-(_grass->getHeight() / 2.0f - _grass->getMargin()), 0.0f, 0.0f);
+    _cannon = new Cannon(cannonPosition);
     _root->addChild(_cannon->getTransform());
 
-    _target = createTarget();
-    _root->addChild(_target);
+    for (int i = 0; i < _numTargets; ++i) {
+        osg::ref_ptr<Target> target = new Target(_grass);
+        _targets.push_back(target);
+        _root->addChild(target->getTransform());
+    }
 }

project_2/src/scene/Scene.h
@@ -5,22 +5,29 @@
 #include "scene/objects/Cannon.h"
 #include "scene/objects/Target.h"
 #include "scene/objects/Projectile.h"
+#include "scene/objects/Grass.h"
 
-#include <osg/ref_ptr>
 #include <osg/Group>
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+#include <vector>
 
-class Scene {
+class Scene : public osg::Referenced {
 public:
     Scene();
     osg::ref_ptr<osg::Group> getRoot() const { return _root; }
 
+    osg::ref_ptr<Grass> getGrass() const { return _grass; }
     osg::ref_ptr<Cannon> getCannon() const { return _cannon; }
-    osg::ref_ptr<osg::PositionAttitudeTransform> getTarget() const { return _target; }
+    std::vector<osg::ref_ptr<Target>>& getTargets() { return _targets; }
 
 private:
     osg::ref_ptr<osg::Group> _root;
+    osg::ref_ptr<Grass> _grass;
     osg::ref_ptr<Cannon> _cannon;
-    osg::ref_ptr<osg::PositionAttitudeTransform> _target;
+    std::vector<osg::ref_ptr<Target>> _targets;
+
+    int _numTargets = 5;
 
     void buildScene();
 };

project_2/src/scene/objects/Cannon.cpp
@@ -1,4 +1,5 @@
 #include "scene/objects/Cannon.h"
+#include "scene/objects/Target.h"
 #include "scene/objects/Projectile.h"
 #include "scene/collisions/ProjectileCollisionCallback.h"
 
@@ -7,14 +8,15 @@
 #include <osg/Quat>
 #include <osg/ShapeDrawable>
 #include <iostream>
+#include <vector>
 
 Cannon::Cannon(const osg::Vec3& position) {
     createCannon(position);
 }
 
 void Cannon::createCannon(const osg::Vec3& position) {
-    _cannonTransform = new osg::PositionAttitudeTransform();
-    _cannonBody = new osg::Geode();
+    this->_cannonTransform = new osg::PositionAttitudeTransform();
+    this->_cannonBody = new osg::Geode();
 
     osg::ref_ptr<osg::ShapeDrawable> cannonShape = new osg::ShapeDrawable(
         new osg::Box(osg::Vec3(0.0f, 0.0f, 0.5f), 1.0f, 1.0f, 2.0f)
@@ -28,32 +30,32 @@ void Cannon::createCannon(const osg::Vec3& position) {
 }
 
 void Cannon::setFireKeyHeld(bool keyHeld) {
-    this->fireKeyHeld = keyHeld;
+    this->_fireKeyHeld = keyHeld;
 }
 
 void Cannon::setFirePressStartTime(double pressStartTime) {
-    this->firePressStartTime = pressStartTime;
+    this->_firePressStartTime = pressStartTime;
 }
 
 void Cannon::setPosition(const osg::Vec3& position) {
-    _cannonTransform->setPosition(position);
+    this->_cannonTransform->setPosition(position);
 }
 
 void Cannon::rotate(float angle, RotationAxis axis) {
     switch (axis) {
     case RotationAxis::YAW:
-        yawAngle += angle;
+        _yawAngle += angle;
 
-        if (yawAngle < minYawAngle) yawAngle = minYawAngle;
-        if (yawAngle > maxYawAngle) yawAngle = maxYawAngle;
+        if (_yawAngle < _minYawAngle) _yawAngle = _minYawAngle;
+        if (_yawAngle > _maxYawAngle) _yawAngle = _maxYawAngle;
 
         break;
 
     case RotationAxis::PITCH:
-        pitchAngle += angle;
+        _pitchAngle += angle;
 
-        if (pitchAngle < minPitchAngle) pitchAngle = minPitchAngle;
-        if (pitchAngle > maxPitchAngle) pitchAngle = maxPitchAngle;
+        if (_pitchAngle < _minPitchAngle) _pitchAngle = _minPitchAngle;
+        if (_pitchAngle > _maxPitchAngle) _pitchAngle = _maxPitchAngle;
 
         break;
 
@@ -62,51 +64,51 @@ void Cannon::rotate(float angle, RotationAxis axis) {
     }
 
     osg::Quat yawQuat(
-        osg::DegreesToRadians(yawAngle),
-        yAxisVector
+        osg::DegreesToRadians(_yawAngle),
+        _yAxisVector
     );
 
     osg::Quat pitchQuat(
-        osg::DegreesToRadians(pitchAngle),
-        zAxisVector
+        osg::DegreesToRadians(_pitchAngle),
+        _zAxisVector
     );
 
     osg::Quat finalQuat = yawQuat * pitchQuat;
 
-    getTransform()->setAttitude(finalQuat);
+    this->getTransform()->setAttitude(finalQuat);
 }
 
-void Cannon::fire(float holdDuration, osg::Group* sceneRoot, osg::PositionAttitudeTransform* target) {
+void Cannon::fire(float holdDuration, osg::Group* sceneRoot, std::vector<osg::ref_ptr<Target>>& targets) {
     if (holdDuration < 0.0f) {
         holdDuration = 0.0f;
-    } else if (holdDuration > maxHoldTime) {
-        holdDuration = maxHoldTime;
+    } else if (holdDuration > _maxHoldTime) {
+        holdDuration = _maxHoldTime;
     }
 
-    float distanceFactor = holdDuration * distancePerHold;
+    float distanceFactor = holdDuration * _distancePerHold;
     
     // std::cout << "Firing cannon! Held for " << holdDuration 
     //           << " seconds. DistanceFactor=" << distanceFactor << std::endl;
 
-    osg::Vec3 cannonPosition = getTransform()->getPosition();
-    osg::Quat cannonRotation = getTransform()->getAttitude();
+    osg::Vec3 cannonPosition = this->getTransform()->getPosition();
+    osg::Quat cannonRotation = this->getTransform()->getAttitude();
 
-    osg::ref_ptr<osg::PositionAttitudeTransform> projectile = createProjectile();
-
-    osg::Vec3 localDirection(zAxisVector);  
+    osg::Vec3 localDirection(_zAxisVector);  
     osg::Vec3 worldDirection = cannonRotation * localDirection;
 
-    float finalDistance = distancePerHold + distanceFactor;
+    float finalDistance = _distancePerHold + distanceFactor;
     osg::Vec3 velocity = worldDirection * finalDistance;
-    osg::ref_ptr<osg::AnimationPath> path = createProjectilePath(cannonPosition, velocity, 15.0f);
 
-    projectile->setUpdateCallback(new osg::AnimationPathCallback(path));
+    osg::ref_ptr<Projectile> projectile = new Projectile(cannonPosition, velocity, 15.0f);
+    osg::ref_ptr<osg::PositionAttitudeTransform> projectileTransform = projectile->getTransform();
+
+    projectileTransform->setUpdateCallback(new osg::AnimationPathCallback(projectile->getAnimationPath()));
 
     osg::ref_ptr<ProjectileCollisionCallback> collisionCb =
-        new ProjectileCollisionCallback(sceneRoot, target);
+        new ProjectileCollisionCallback(sceneRoot, targets, projectile);
 
-    collisionCb->addNestedCallback(projectile->getUpdateCallback());
-    projectile->setUpdateCallback(collisionCb);
+    collisionCb->addNestedCallback(projectileTransform->getUpdateCallback());
+    projectileTransform->setUpdateCallback(collisionCb);
 
-    sceneRoot->addChild(projectile);
+    sceneRoot->addChild(projectileTransform);
 }

project_2/src/scene/objects/Cannon.h
@@ -1,6 +1,8 @@
 #ifndef CANNON_H
 #define CANNON_H
 
+#include "scene/objects/Target.h"
+
 #include <osg/PositionAttitudeTransform>
 #include <osg/Group>
 #include <osg/ref_ptr>
@@ -12,45 +14,45 @@ class Cannon : public osg::Referenced {
 public:
     Cannon(const osg::Vec3& position = osg::Vec3(-20.0f, 0.0f, 0.0f));
 
+    static constexpr float ROTATE_CANNON_ANGLE = 2.5f;
+
     enum RotationAxis {
         YAW,
         PITCH
     };
 
-    static constexpr float ROTATE_CANNON_ANGLE = 5.0f;
-
-    osg::ref_ptr<osg::PositionAttitudeTransform> getTransform() const { return _cannonTransform; }
-    bool getFireKeyHeld() { return fireKeyHeld; };
-    double getFirePressStartTime() { return firePressStartTime; };
+    osg::ref_ptr<osg::PositionAttitudeTransform> getTransform() const { return this->_cannonTransform; }
+    bool getFireKeyHeld() const { return this->_fireKeyHeld; };
+    double getFirePressStartTime() const { return this->_firePressStartTime; };
 
     void setFireKeyHeld(bool keyHeld);
     void setFirePressStartTime(double pressStartTime);
     void setPosition(const osg::Vec3& position);
 
     void rotate(float angle, RotationAxis axis);
-    void fire(float holdDuration, osg::Group* sceneRoot, osg::PositionAttitudeTransform* target);
+    void fire(float holdDuration, osg::Group* sceneRoot, std::vector<osg::ref_ptr<Target>>& targets);
 
 private:
     osg::ref_ptr<osg::PositionAttitudeTransform> _cannonTransform;
     osg::ref_ptr<osg::Geode> _cannonBody;
 
-    const osg::Vec3 yAxisVector = osg::Vec3(0.0f, 1.0f, 0.0f);
-    const osg::Vec3 zAxisVector = osg::Vec3(0.0f, 0.0f, 1.0f);
+    const osg::Vec3 _yAxisVector = osg::Vec3(0.0f, 1.0f, 0.0f);
+    const osg::Vec3 _zAxisVector = osg::Vec3(0.0f, 0.0f, 1.0f);
 
-    const float maxHoldTime     = 3.0f;
-    const float distancePerHold = 7.5f;
+    const float _maxHoldTime     = 3.0f;
+    const float _distancePerHold = 7.5f;
 
-    const float minPitchAngle = -45.0f;
-    const float maxPitchAngle = 45.0f;
-    const float minYawAngle   = 5.0f;
-    const float maxYawAngle   = 45.0f;
+    const float _minPitchAngle = -45.0f;
+    const float _maxPitchAngle = 45.0f;
+    const float _minYawAngle   = 5.0f;
+    const float _maxYawAngle   = 45.0f;
 
-    float pitchAngle = 0.0f; // up-down
-    float yawAngle   = 0.0f; // left-right
+    float _pitchAngle = 0.0f; // up-down
+    float _yawAngle   = 0.0f; // left-right
 
-    double firePressStartTime = 0.0;
+    double _firePressStartTime = 0.0;
 
-    bool fireKeyHeld = false;
+    bool _fireKeyHeld = false;
 
     void createCannon(const osg::Vec3& position);
 };

project_2/src/scene/objects/Projectile.h
@@ -3,10 +3,33 @@
 
 #include <osg/PositionAttitudeTransform>
 #include <osg/AnimationPath>
+#include <osg/Referenced>
+#include <osg/Vec3>
+#include <osg/Geode>
+#include <osg/ref_ptr>
 
-static const float PROJECTILE_RADIUS = 0.25f;
+class Projectile : public osg::Referenced {
+public:
+    Projectile(const osg::Vec3& startPosition, const osg::Vec3& velocity, float duration);
 
-osg::ref_ptr<osg::PositionAttitudeTransform> createProjectile();
-osg::ref_ptr<osg::AnimationPath> createProjectilePath(const osg::Vec3& start, const osg::Vec3& velocity, float duration);
+    osg::ref_ptr<osg::PositionAttitudeTransform> getTransform() const { return _projectileTransform; }
+    osg::ref_ptr<osg::AnimationPath> getAnimationPath() const { return _animationPath; }
+    osg::Vec3 getPosition() const { return _projectileTransform->getPosition(); }
+    float getProjectileRadius() const { return _projectileRadius; }
+    float getProjectileGravity() const { return _projectileGravity; }
+
+    void setPosition(const osg::Vec3& position);
+
+private:
+    osg::ref_ptr<osg::PositionAttitudeTransform> _projectileTransform;
+    osg::ref_ptr<osg::Geode> _projectileBody;
+    osg::ref_ptr<osg::AnimationPath> _animationPath;
+
+    const float _projectileRadius  = 0.25f;
+    const float _projectileGravity = -9.8f;
+
+    void createProjectile();
+    void createProjectilePath(const osg::Vec3& start, const osg::Vec3& velocity, float duration);
+};
 
 #endif // PROJECTILE_H

project_2/src/Main.cpp
@@ -1,20 +1,21 @@
 #include "scene/Scene.h"
 #include "scene/objects/Cannon.h"
+#include "scene/objects/Target.h"
 #include "input/InputHandler.h"
 
 #include <osgViewer/Viewer>
 
 int main(int argc, char** argv) {
-    Scene scene;
-    osg::ref_ptr<osg::Group> root = scene.getRoot();
+    osg::ref_ptr<Scene> scene = new Scene();
+    osg::ref_ptr<osg::Group> root = scene->getRoot();
 
-    osg::ref_ptr<Cannon> cannon = scene.getCannon();
-    auto targetTransform = dynamic_cast<osg::PositionAttitudeTransform*>(root->getChild(2));
+    osg::ref_ptr<Cannon> cannon = scene->getCannon();
+    std::vector<osg::ref_ptr<Target>>& targets = scene->getTargets();
 
     osgViewer::Viewer viewer;
     viewer.setSceneData(root);
 
-    osg::ref_ptr<InputHandler> inputHandler = new InputHandler(root, cannon, targetTransform);
+    osg::ref_ptr<InputHandler> inputHandler = new InputHandler(root, cannon, targets);
     viewer.addEventHandler(inputHandler);
 
     return viewer.run();

commit a36f0fe11982e16f6af770ef2c6a44c89bcdaf20

Additional refactoring
- Fixed issue with target spawns introduced in d90b631
- Created new methods in Scene object that
  make the code cleaner
- Added cannon helper path before firing a projectile
- [NEED-FIX] Helper path's rotation seems to be fixed on 1 axis

project_2/CMakeLists.txt
@@ -10,7 +10,7 @@ file(GLOB_RECURSE SOURCES CONFIGURE_DEPENDS ${CMAKE_SOURCE_DIR}/src/*.cpp)
 
 add_executable(game ${SOURCES})
 
-target_link_libraries(game osgDB osgGA osgViewer osgUtil osg)
+target_link_libraries(game osg osgDB osgGA osgViewer osgUtil)
 target_include_directories(game PRIVATE ${CMAKE_SOURCE_DIR}/src)
 
 message(STATUS "Found source files: ${SOURCES}")

project_2/src/input/InputHandler.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 bool InputHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa) {
+    float holdDuration;
+
     switch (ea.getEventType()) {
     case osgGA::GUIEventAdapter::KEYDOWN:
         switch(ea.getKey()) {
@@ -12,7 +14,13 @@ bool InputHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdap
             if (!_cannon->getFireKeyHeld()) {
                 _cannon->setFireKeyHeld(true);
                 _cannon->setFirePressStartTime(ea.getTime());
+                _cannon->showHelperPath();
             }
+
+            holdDuration = ea.getTime() - _cannon->getFirePressStartTime();
+            holdDuration = clampHoldDuration(holdDuration, _cannon->getMaxHoldDuration());
+            _cannon->createHelperPath(holdDuration);
+
             return true;
 
         case osgGA::GUIEventAdapter::KEY_W:
@@ -43,9 +51,11 @@ bool InputHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdap
         if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Space && _cannon->getFireKeyHeld()) {
             // std::cout << "Key Space released: Firing cannon." << std::endl;
             _cannon->setFireKeyHeld(false);
-            double holdDuration = ea.getTime() - _cannon->getFirePressStartTime();
+            _cannon->hideHelperPath();
+            holdDuration = ea.getTime() - _cannon->getFirePressStartTime();
+            holdDuration = clampHoldDuration(holdDuration, _cannon->getMaxHoldDuration());
 
-            _cannon->fire(static_cast<float>(holdDuration), _sceneRoot, _targets);
+            _cannon->fire(holdDuration, _sceneRoot, _targets);
 
             return true;
         }
@@ -55,4 +65,10 @@ bool InputHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdap
     }
 
     return false;
+}
+
+float InputHandler::clampHoldDuration(float duration, float maxHoldTime) {
+    if (duration < 0.0f)        { return 0.0f; }
+    if (duration > maxHoldTime) { return maxHoldTime; }
+    return duration;
 }

project_2/src/input/InputHandler.h
@@ -22,6 +22,8 @@ private:
     osg::Group* _sceneRoot;
     osg::ref_ptr<Cannon> _cannon;
     std::vector<osg::ref_ptr<Target>> _targets;
+
+    float clampHoldDuration(float holdDuration, float maxHoldTime);
 };
 
 #endif // INPUT_HANDLER_H

project_2/src/scene/Scene.cpp
@@ -2,25 +2,60 @@
 #include "scene/objects/Grass.h"
 #include "scene/objects/Cannon.h"
 #include "scene/objects/Target.h"
+// #include "ui/UI.h"
 
 #include <osg/Vec3>
 
 Scene::Scene() {
-    _root = new osg::Group();
+    this->_root = new osg::Group();
     buildScene();
 }
 
 void Scene::buildScene() {
-    _grass = new Grass();
+    addGrass();
+
+    addCannon();
+
+    addTargets();
+
+    // _ui = new UI("Hello OSG!", 50, 700);
+    // _root->addChild(_ui->getUIElement());
+}
+
+void Scene::addGrass() {
+    this->_grass = new Grass();
     _root->addChild(_grass->getTransform());
+}
 
+void Scene::addCannon() {
     osg::Vec3 cannonPosition = osg::Vec3(-(_grass->getHeight() / 2.0f - _grass->getMargin()), 0.0f, 0.0f);
-    _cannon = new Cannon(cannonPosition);
+    this->_cannon = new Cannon(cannonPosition);
     _root->addChild(_cannon->getTransform());
+}
 
+void Scene::addTargets() {
     for (int i = 0; i < _numTargets; ++i) {
-        osg::ref_ptr<Target> target = new Target(_grass);
-        _targets.push_back(target);
+        osg::ref_ptr<Target> target;
+        bool validPosition = false;
+
+        while (!validPosition) {
+            target = new Target(_grass);
+
+            osg::Vec3 newPosition = target->getTransform()->getPosition();
+            validPosition = true;
+
+            for (const auto& existingTarget : _targets) {
+                osg::Vec3 existingPosition = existingTarget->getTransform()->getPosition();
+                float distance = (newPosition - existingPosition).length();
+
+                if (distance < _minTargetDistance) {
+                    validPosition = false;
+                    break;
+                }
+            }
+        }
+
+        this->_targets.push_back(target);
         _root->addChild(target->getTransform());
     }
 }

project_2/src/scene/Scene.h
@@ -6,6 +6,7 @@
 #include "scene/objects/Target.h"
 #include "scene/objects/Projectile.h"
 #include "scene/objects/Grass.h"
+// #include "ui/UI.h"
 
 #include <osg/Group>
 #include <osg/Referenced>
@@ -26,10 +27,16 @@ private:
     osg::ref_ptr<Grass> _grass;
     osg::ref_ptr<Cannon> _cannon;
     std::vector<osg::ref_ptr<Target>> _targets;
+    // osg::ref_ptr<UI> _ui;
 
     int _numTargets = 5;
 
+    float _minTargetDistance = 5.0f;
+
     void buildScene();
+    void addGrass();
+    void addCannon();
+    void addTargets();
 };
 
 #endif // SCENE_H

project_2/src/scene/objects/Cannon.cpp
@@ -17,6 +17,7 @@ Cannon::Cannon(const osg::Vec3& position) {
 void Cannon::createCannon(const osg::Vec3& position) {
     this->_cannonTransform = new osg::PositionAttitudeTransform();
     this->_cannonBody = new osg::Geode();
+    this->_helperPathGroup = new osg::Group();
 
     osg::ref_ptr<osg::ShapeDrawable> cannonShape = new osg::ShapeDrawable(
         new osg::Box(osg::Vec3(0.0f, 0.0f, 0.5f), 1.0f, 1.0f, 2.0f)
@@ -79,12 +80,6 @@ void Cannon::rotate(float angle, RotationAxis axis) {
 }
 
 void Cannon::fire(float holdDuration, osg::Group* sceneRoot, std::vector<osg::ref_ptr<Target>>& targets) {
-    if (holdDuration < 0.0f) {
-        holdDuration = 0.0f;
-    } else if (holdDuration > _maxHoldTime) {
-        holdDuration = _maxHoldTime;
-    }
-
     float distanceFactor = holdDuration * _distancePerHold;
     
     // std::cout << "Firing cannon! Held for " << holdDuration 
@@ -111,4 +106,96 @@ void Cannon::fire(float holdDuration, osg::Group* sceneRoot, std::vector<osg::re
     projectileTransform->setUpdateCallback(collisionCb);
 
     sceneRoot->addChild(projectileTransform);
+}
+
+void Cannon::createHelperPath(float duration) {
+    _cannonTransform->removeChild(_helperPathGroup);  // Remove old helper path
+
+    this->_helperPathGroup = new osg::Group();
+
+    osg::Vec3 start    = osg::Vec3(0.0f, 0.0f, 2.0f);  // Position is relative to the cannon
+    osg::Quat rotation = this->_cannonTransform->getAttitude();
+    // osg::Quat rotation = osg::Quat(0.0f, 0.0f, 0.0f, 0.0f);
+
+    float distanceFactor = duration * _distancePerHold;
+
+    osg::Vec3 localDirection(_zAxisVector);
+    osg::Vec3 worldDirection = rotation * localDirection;
+
+    float finalDistance = _distancePerHold + distanceFactor;
+    osg::Vec3 velocity = worldDirection * finalDistance;
+
+    const int numSteps = 20;
+    float x, y, z;
+    float timeToGroundHit = computeTimeToGround(start, velocity, Projectile::getProjectileGravity());
+    for (int i = 0; i <= numSteps; ++i) {
+        float t = timeToGroundHit * static_cast<float>(i) / numSteps;
+        float x = start.x() + velocity.x() * t;
+        float y = start.y() + velocity.y() * t;
+        float z = start.z() + velocity.z() * t + 0.5f * Projectile::getProjectileGravity() * t * t;
+
+        osg::Vec3 position(x, y, z);
+
+        osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+        osg::ref_ptr<osg::ShapeDrawable> sphereDrawable = new osg::ShapeDrawable(
+            new osg::Sphere(osg::Vec3(0.0, 0.0, 0.0), 0.1f)
+        );
+        sphereDrawable->setColor(osg::Vec4(1.0, 1.0, 0.0, 1.0));
+
+        geode->addDrawable(sphereDrawable);
+
+        osg::ref_ptr<osg::PositionAttitudeTransform> helperTransform = new osg::PositionAttitudeTransform();
+        helperTransform->setPosition(position);
+        helperTransform->addChild(geode);
+
+        _helperPathGroup->addChild(helperTransform);
+    }
+
+    _cannonTransform->addChild(_helperPathGroup);
+}
+
+void Cannon::showHelperPath() {
+    if (!_helperPathVisible) {
+        // std::cout << "Showing helper path." << std::endl;
+        _helperPathGroup->setNodeMask(~0x0);
+        _helperPathVisible = true;
+    }
+}
+
+void Cannon::hideHelperPath() {
+    if (_helperPathVisible) {
+        // std::cout << "Hiding helper path." << std::endl;
+        _helperPathGroup->setNodeMask(0x0);
+        _helperPathVisible = false;
+    }
+}
+
+float Cannon::computeTimeToGround(const osg::Vec3& start, const osg::Vec3& velocity, float gravity)
+{
+    float z0 = start.z();
+    float vz = velocity.z();
+    float a = 0.5f * gravity;
+    float b = vz;
+    float c = z0;
+
+    float discriminant = b*b - 4.0f*a*c;
+    if (discriminant < 0.0f) {
+        return -1.0f;
+    }
+
+    float sqrtDisc = std::sqrt(discriminant);
+
+    float t1 = (-b + sqrtDisc) / (2.0f * a);
+    float t2 = (-b - sqrtDisc) / (2.0f * a);
+
+    float tPos = -1.0f;
+    if (t1 >= 0.0f && t2 >= 0.0f) {
+        tPos = std::min(t1, t2);
+    } else if (t1 >= 0.0f) {
+        tPos = t1;
+    } else if (t2 >= 0.0f) {
+        tPos = t2;
+    }
+
+    return tPos;
 }

project_2/src/scene/objects/Cannon.h
@@ -24,6 +24,7 @@ public:
     osg::ref_ptr<osg::PositionAttitudeTransform> getTransform() const { return this->_cannonTransform; }
     bool getFireKeyHeld() const { return this->_fireKeyHeld; };
     double getFirePressStartTime() const { return this->_firePressStartTime; };
+    float getMaxHoldDuration() const { return this->_maxHoldTime; }
 
     void setFireKeyHeld(bool keyHeld);
     void setFirePressStartTime(double pressStartTime);
@@ -32,9 +33,14 @@ public:
     void rotate(float angle, RotationAxis axis);
     void fire(float holdDuration, osg::Group* sceneRoot, std::vector<osg::ref_ptr<Target>>& targets);
 
+    void createHelperPath(float duration);
+    void showHelperPath();
+    void hideHelperPath();
+
 private:
     osg::ref_ptr<osg::PositionAttitudeTransform> _cannonTransform;
     osg::ref_ptr<osg::Geode> _cannonBody;
+    osg::ref_ptr<osg::Group> _helperPathGroup;
 
     const osg::Vec3 _yAxisVector = osg::Vec3(0.0f, 1.0f, 0.0f);
     const osg::Vec3 _zAxisVector = osg::Vec3(0.0f, 0.0f, 1.0f);
@@ -52,9 +58,12 @@ private:
 
     double _firePressStartTime = 0.0;
 
-    bool _fireKeyHeld = false;
+    bool _fireKeyHeld       = false;
+    bool _helperPathVisible = false;
 
     void createCannon(const osg::Vec3& position);
+
+    float computeTimeToGround(const osg::Vec3& start, const osg::Vec3& velocity, float gravity);
 };
 
 #endif // CANNON_H

project_2/src/scene/objects/Projectile.h
@@ -12,11 +12,12 @@ class Projectile : public osg::Referenced {
 public:
     Projectile(const osg::Vec3& startPosition, const osg::Vec3& velocity, float duration);
 
+    static float getProjectileGravity() { return _projectileGravity; }
+
     osg::ref_ptr<osg::PositionAttitudeTransform> getTransform() const { return _projectileTransform; }
     osg::ref_ptr<osg::AnimationPath> getAnimationPath() const { return _animationPath; }
     osg::Vec3 getPosition() const { return _projectileTransform->getPosition(); }
     float getProjectileRadius() const { return _projectileRadius; }
-    float getProjectileGravity() const { return _projectileGravity; }
 
     void setPosition(const osg::Vec3& position);
 
@@ -25,8 +26,9 @@ private:
     osg::ref_ptr<osg::Geode> _projectileBody;
     osg::ref_ptr<osg::AnimationPath> _animationPath;
 
+    static constexpr float _projectileGravity = -9.8f;
+
     const float _projectileRadius  = 0.25f;
-    const float _projectileGravity = -9.8f;
 
     void createProjectile();
     void createProjectilePath(const osg::Vec3& start, const osg::Vec3& velocity, float duration);